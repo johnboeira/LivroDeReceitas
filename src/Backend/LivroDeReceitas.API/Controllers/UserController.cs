c49bc5c68f4478786c919e3ff9688a599f168117
johnBoeira <johnboeira@gmail.com>
2025-03-01 13:45:47 -0300

    feat: Adiciona controlador de usuário

---
+++ src/Backend/LivroDeReceitas.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using LivroDeReceitas.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LivroDeReceitas.API;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(UserRegisterDTO),StatusCodes.Status201Created)]
+    public IActionResult Register()
+    {
+        return Created();
+    }
+}
