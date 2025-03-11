ec27e0907bd1cce81143d6dcd2ed5bac89343c33
johnBoeira <johnboeira@gmail.com>
2025-03-11 19:46:30 -0300

    feat: Adiciona filtro para exceptions

---
+++ src/Backend/LivroDeReceitas.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using LivroDeReceitas.Exceptions.ExecptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+
+namespace LivroDeReceitas.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is LivroDeReceitasException)
+        {
+            HandlerProjectException(context);
+        }
+        else
+        {
+
+        }
+    }
+
+    private void HandlerProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        }
+    }
+}
